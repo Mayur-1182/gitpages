f28279ac8ede4cf121b40ecfa3359f0ff59c87d0
MAYUR PARMAR <parmar.mayurumb11@gmail.com>
2024-07-10 14:42:17 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 function App() {
   return (
-    <div>
+    <div className="bg-yellow-200">
       <h1>Github pages live is here</h1>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos culpa
       nam ex?
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, eum!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae maxime
+        consectetur labore minima maiores ex?
+      </p>
     </div>
   );
 }
