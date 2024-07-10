052cfff8cd0145c8d3f7407f521436a58ed1bdeb
MAYUR PARMAR <parmar.mayurumb11@gmail.com>
2024-07-10 13:11:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos culpa
+      nam ex?
+    </div>
+  );
+}
+
+export default App;
